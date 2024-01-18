5c35e18a02275d79cc0cacadc478cf944a8fc3d0
Samuel Anijar <samuelb.anijar@gmail.com>
2024-01-18 07:18:41 -0400

    resolução do desafio

--- app/src/main/java/br/com/igorbag/githubsearch/domain/Repository.kt
+++ app/src/main/java/br/com/igorbag/githubsearch/domain/Repository.kt
@@ -5,5 +5,5 @@ import com.google.gson.annotations.SerializedName
 data class Repository(
     val name: String,
     @SerializedName("html_url")
-    val htmlUrl: String
+    val htmlUrl: String,
 )
